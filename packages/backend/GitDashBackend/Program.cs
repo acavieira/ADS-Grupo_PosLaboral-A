27381bdbfad3a141e48cc9f850ec0e92a00198f3
Jéssica Vieira <jacva@iscte-iul.pt>
2025-11-01 17:58:08 +0000

    Initial Authentication commit

--- packages/backend/GitDashBackend/Program.cs
+++ packages/backend/GitDashBackend/Program.cs
@@ -1,19 +1,59 @@
 using GitDashBackend.Configurations;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OAuth;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+//Add services to the container
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
-// Register custom services
 builder.Services.AddProjectServices();
+
+//Configure authentication with GitHub
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = "GitHub";
+})
+.AddCookie()
+.AddOAuth("GitHub", options =>
+{
+    options.ClientId = builder.Configuration["GitHub:ClientId"];
+    options.ClientSecret = builder.Configuration["GitHub:ClientSecret"];
+    options.CallbackPath = new PathString("/signin-github");
+
+    options.AuthorizationEndpoint = "https://github.com/login/oauth/authorize";
+    options.TokenEndpoint = "https://github.com/login/oauth/access_token";
+    options.UserInformationEndpoint = "https://api.github.com/user";
+
+    options.SaveTokens = true;
+
+    options.ClaimActions.MapJsonKey(System.Security.Claims.ClaimTypes.NameIdentifier, "id");
+    options.ClaimActions.MapJsonKey(System.Security.Claims.ClaimTypes.Name, "login");
+    options.ClaimActions.MapJsonKey(System.Security.Claims.ClaimTypes.Email, "email");
+
+    options.Events = new OAuthEvents
+    {
+        OnCreatingTicket = async context =>
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
+            request.Headers.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", context.AccessToken);
+
+            var response = await context.Backchannel.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            var user = System.Text.Json.JsonDocument.Parse(await response.Content.ReadAsStringAsync());
+            context.RunClaimActions(user.RootElement);
+        }
+    };
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+//Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -21,8 +61,31 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+//Middleware of authentication and authorization
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+//test route
+app.MapGet("/", async context =>
+{
+    if (context.User.Identity?.IsAuthenticated ?? false)
+    {
+        await context.Response.WriteAsync($"Olá, {context.User.Identity.Name}!");
+    }
+    else
+    {
+        context.Response.Redirect("/login");
+    }
+});
+
+app.MapGet("/login", async context =>
+{
+    await context.ChallengeAsync("GitHub", new AuthenticationProperties
+    {
+        RedirectUri = "/"
+    });
+});
 
 app.Run();
