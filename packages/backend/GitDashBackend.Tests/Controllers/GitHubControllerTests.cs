063d8059413173f090f5c7143f1e7fdd52b637d8
Vasco C. <vrceo@iscte-iul.pt>
2025-11-07 00:31:56 +0000

    feat: add first tests

---
+++ packages/backend/GitDashBackend.Tests/Controllers/GitHubControllerTests.cs
@@ -0,0 +1,93 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using GitDashBackend.Controllers;
+using GitDashBackend.Domain.DTOs;
+using GitDashBackend.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using Octokit;
+using Octokit.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GitDashBackend.Tests.Controllers
+{
+    public class GitHubControllerTests
+    {
+        private readonly IGitHubService _gitHubService;
+        private readonly ILogger<GitHubController> _logger;
+        private readonly GitHubController _controller;
+
+        public GitHubControllerTests()
+        {
+            _gitHubService = A.Fake<IGitHubService>();
+            _logger = A.Fake<ILogger<GitHubController>>();
+            _controller = new GitHubController(_gitHubService, _logger);
+        }
+
+        // -------------------------------
+        // GetRepositories Tests
+        // -------------------------------
+
+        [Fact]
+        public async Task GetRepositories_ShouldReturnBadRequest_WhenTokenMissing()
+        {
+            // Act
+            var result = await _controller.GetRepositories(null);
+
+            // Assert
+            var badRequest = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal(StatusCodes.Status400BadRequest, badRequest.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetRepositories_ShouldReturnOk_WhenRepositoriesFound()
+        {
+            // Arrange
+            RepositoriesDto fakeRepos = new();
+            fakeRepos.count = 2;
+            fakeRepos.repositories.Add(new RepositoryDto());
+            fakeRepos.repositories.Add(new RepositoryDto());
+
+            A.CallTo(() => _gitHubService.GetUserRepositoriesAsync("token"))
+                .Returns(Task.FromResult<RepositoriesDto>(fakeRepos));
+
+            // Act
+            GitHubController controller_ = new GitHubController(_gitHubService, _logger);
+            var okObjectResult = await controller_.GetRepositories("token") as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(okObjectResult);
+
+            Assert.IsType<OkObjectResult>(okObjectResult);
+
+            var repositories = (RepositoriesDto)okObjectResult.Value;
+          
+            Assert.Equal(2, repositories.count);
+
+        }
+
+        [Fact]
+        public async Task GetRepositories_ShouldReturnInternalServerError_WhenUnhandledException()
+        {
+            // Arrange
+            A.CallTo(() => _gitHubService.GetUserRepositoriesAsync("token"))
+                .ThrowsAsync(new Exception("Unexpected"));
+
+            // Act
+            var result = await _controller.GetRepositories("token");
+
+            // Assert
+            var error = Assert.IsType<ObjectResult>(result);
+            Assert.Equal(StatusCodes.Status500InternalServerError, error.StatusCode);
+        }
+
+    }
+}
