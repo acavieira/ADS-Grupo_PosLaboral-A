ea3df6e3281da955c952a85d1f571d33fa4615cd
Vasco C. <vrceo@iscte-iul.pt>
2025-11-10 13:37:39 +0000

    feat(backend): add new stats dtos

---
+++ packages/backend/GitDashBackend/Domain/DTOs/OpenWorkDto.cs
@@ -0,0 +1,8 @@
+﻿namespace GitDashBackend.Domain.DTOs;
+
+public class OpenWorkDto
+{
+    public int OpenPrs { get; set; }
+    public int OpenIssues { get; set; }
+    public int NeedsReview { get; set; }
+}
